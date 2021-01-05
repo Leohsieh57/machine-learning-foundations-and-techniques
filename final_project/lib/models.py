c326125cf45765b04cc5fb8b09bb26ecaf9e5005
Leohsieh57 <bardiche4768@gmail.com>
2021-01-05 15:33:35 +0800

    test model

---
+++ final_project/lib/models.py
@@ -0,0 +1,39 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset, DataLoader
+import glob
+import os
+import numpy as np
+from PIL import Image
+import torchvision.models as models
+import copy
+from torchvision.utils import save_image
+import PIL
+import pandas as pd
+import matplotlib.pyplot as plt
+from lib import preprocessor as prep
+import csv
+import multiprocessing as mp
+import copy
+import threading
+import random
+
+class Classifier(nn.Module):
+    def __init__(self, input_size = 258):
+        super(Classifier, self).__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(input_size, 180),
+            nn.Dropout(0.5),
+            nn.ReLU(),
+            nn.Linear(input_size, 100),
+            nn.Dropout(0.5),
+            nn.ReLU(),
+            nn.Linear(100, 2)
+        )
+
+    def forward(self, x):
+        return self.fc(x)
