18a79dd6bcf46e9c4a4c484c8e99f5a544ffe8d2
Leohsieh57 <bardiche4768@gmail.com>
2021-01-05 15:24:46 +0800

    loader

---
+++ final_project/lib/data_loader.py
@@ -0,0 +1,40 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset, DataLoader
+import glob
+import os
+import numpy as np
+from PIL import Image
+import torchvision.models as models
+import copy
+from torchvision.utils import save_image
+import PIL
+import skimage.io
+import multiprocessing as mp
+import time
+import pandas as pd
+import matplotlib.pyplot as plt
+import random
+
+class HotelReservationData(Dataset):
+    def __init__(self, root):
+        self.root = root
+        self.data = torch.load(os.path.join(self.root, 'data.pt'))
+        self.adr = torch.load(os.path.join(self.root, 'adr.pt'))
+        self.rev = torch.load(os.path.join(self.root, 'rev.pt'), dtype=torch.int32)
+        assert(len(self.adr) == len(self.rev))
+        assert(len(self.adr) == len(self.data))
+        self.len = len(self.adr)
+    
+    def __getitem__(self, i):
+        data = self.data[i]
+        adr = self.adr[i]
+        rev = self.rev[i]
+        return data, adr, rev
+    
+    def __len__(self):
+        return self.len
---
+++ final_project/lib/model_device_io.py
@@ -0,0 +1,40 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset, DataLoader
+import glob
+import os
+import numpy as np
+from PIL import Image
+import torchvision.models as models
+import copy
+from torchvision.utils import save_image
+import PIL
+import pandas as pd
+import matplotlib.pyplot as plt
+import random
+
+def saveModel(checkpoint_path, model, optimizer):
+    state = {'state_dict': model.state_dict(),
+             'optimizer' : optimizer.state_dict()}
+    torch.save(state, checkpoint_path)
+    print('model saved to %s' % checkpoint_path)
+    
+def loadModel(checkpoint_path, model, optimizer, log=True):
+    state = torch.load(checkpoint_path)
+    model.load_state_dict(state['state_dict'])
+    optimizer.load_state_dict(state['optimizer'])
+    if log:
+        print('model loaded from %s' % checkpoint_path)
+    
+def getCudaDevice(cudaNum = 1, torchSeed = 123):
+    use_cuda = torch.cuda.is_available()
+    torch.manual_seed(torchSeed)
+    deviceName = "cuda:"+str(cudaNum)
+    device = torch.device(deviceName if use_cuda else "cpu")
+    #device = torch.device('cpu')
+    print('Device used:', device)
+    return device
