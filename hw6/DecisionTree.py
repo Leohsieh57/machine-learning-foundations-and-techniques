b86c692b9495bc177dd97a590eb02fa0398b3ee4
Leohsieh57 <bardiche4768@gmail.com>
2021-01-14 22:25:18 +0800

    0.295

--- hw6/DecisionTree.py
+++ hw6/DecisionTree.py
@@ -29,6 +29,21 @@ class DecisionTree:
 
             self.BinaryDict[OutcomeString].append(label)
            
+        for depth in range(self.depth):
+            NewKeyDict = {}
+            for key in self.BinaryDict:
+                if len(key) != self.depth - depth:
+                    continue
+
+                NewKey = key[:-1]
+                if not NewKeyDict.get(NewKey):
+                    NewKeyDict[NewKey] = []
+
+                NewKeyDict[NewKey] += self.BinaryDict[key]
+
+            for NewKey in NewKeyDict:
+                self.BinaryDict[NewKey] = NewKeyDict[NewKey]
+
         for key in self.BinaryDict:
             self.BinaryDict[key] = sign(sum(self.BinaryDict[key]))
 
@@ -41,13 +56,16 @@ class DecisionTree:
                 x = feat[idx]
                 OutcomeList.append(model.Predict(x))
 
-            OutcomeString = OutcomeListToLabel(OutcomeList)
-            Prediction = self.BinaryDict.get(OutcomeString)
-            Prediction = Prediction if Prediction else -1
-            PredictionList.append(Prediction)
+            OutcomeString = OutcomeListToLabel(OutcomeList)+'_'
+            for depth in range(self.depth):
+                key = OutcomeString[:-(depth+1)]
+                Prediction = self.BinaryDict.get(key)
+                if Prediction != None:
+                    PredictionList.append(Prediction)
+                    break
+            
         return PredictionList
     
-
 def OutcomeListToLabel(OutcomeList):
     OutputString = ''
     for outcome in OutcomeList:
